5646b05dc13ceddb30faed2a330343500b3c98e4
iyan-devcore <memoniyan@gmail.com>
2026-05-19 17:49:26 +0530

    commit backup

--- src/features/dashboard/views/dashboard-view.tsx
+++ src/features/dashboard/views/dashboard-view.tsx
@@ -1,9 +1,17 @@
+import { PageHeader } from "@/components/page-header";
 import { HeroPattern } from "@/features/dashboard/components/hero-patten";
+import { DashboardHeader } from "../components/dashboard-header";
+import { TextInputPanel } from "../components/text-input-panel";
 
 export function DashboardView(){
     return(
         <div className="relative">
+            <PageHeader title="Dashboard" className="lg:hidden" />
             <HeroPattern/>
+            <div className="relative space-y-8 p-4 lg:p-16">
+                <DashboardHeader />
+                <TextInputPanel />
+            </div>
         </div>
     )
 }
